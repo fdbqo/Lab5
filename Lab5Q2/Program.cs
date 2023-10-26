655ef38b0068048194ad59cb5ff5fd28184300c6
Finn De Bie <115426568+fdbqo@users.noreply.github.com>
2023-10-26 10:40:09 +0000

    final

---
+++ Lab5Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab5Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence1 = "one two three four five";
+            string sentence2 = "one two! three four? five, six seven eight nine ten";
+            string sentence3 = "one two three four, five six seven";
+
+            Console.WriteLine($"Sentence 1 word count: {sentence1.WordCount()}");
+            Console.WriteLine($"Sentence 2 word count: {sentence2.WordCount()}");
+            Console.WriteLine($"Sentence 3 word count: {sentence3.WordCount()}");
+        }
+
+        
+    }
+
+    public static class StringExtensions 
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
